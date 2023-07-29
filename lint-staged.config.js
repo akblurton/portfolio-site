7c777de48865584b9d1f529edbf52e472a9fcce1
Ashley Blurton <4752876+akblurton@users.noreply.github.com>
2023-07-29 17:31:19 +0100

    chore: 🔧 correct linting setup

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,11 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
 module.exports = {
-  "*.{ts,tsx,js,jsx,md,mdx}": "prettier --write",
-  "*.{ts,tsx}": "eslint --fix",
+  "*.{ts,tsx,js,jsx,md,mdx}": ["prettier --write"],
+  "*.{ts,tsx}": [buildEslintCommand],
 };

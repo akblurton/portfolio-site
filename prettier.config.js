911af0c90d88af636d3b61edac30915ef487cff6
Ashley Blurton <4752876+akblurton@users.noreply.github.com>
2023-07-30 15:27:41 +0100

    chore: 🔧 use different prettier config format for vscode

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-
-/** @type {import('prettier').Config} */
-module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
-  tailwindConfig: path.resolve(__dirname, "./tailwind.config.js"),
-};
